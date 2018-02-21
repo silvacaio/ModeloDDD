492085c14fdc5bd387d776a9ad23d332af1e3702
Caio Silva <silva.caiosfc@hotmail.com>
2018-02-21 13:09:40 -0300

    Add project files.

---
+++ ModeloDDD.Infra.Data/Context/ModeloDDDContext.cs
@@ -0,0 +1,15 @@
+﻿using ModeloDDD.Domain.Entities;
+using System.Data.Entity;
+
+namespace ModeloDDD.Infra.Data.Context
+{
+    public class ModeloDDDContext : DbContext
+    {
+        public ModeloDDDContext() : base("ModeloDDD")
+        {
+
+        }
+
+        public DbSet<Cliente> Clientes { get; set; }
+    }
+}
